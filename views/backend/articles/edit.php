4e9eaf97626ada960a78833298ef1cdef4bd86ba
AmelieSavaryTolstoy <tolstoy.a.savary@gmail.com>
2025-02-04 10:32:28 +0100

    Update edit.php
    coquille

--- views/backend/articles/edit.php
+++ views/backend/articles/edit.php
@@ -39,7 +39,7 @@ $article = sql_select($query, "*") [0];
         </div>
         <div class="col-md-12">
             <!-- Form to create a new statut -->
-            <form action="<?php echo ROOT_URL . '/api/karticles/update.php' ?>" method="post">
+            <form action="<?php echo ROOT_URL . '/api/articles/update.php' ?>" method="post">
                 <div class="form-group">
                     <label for="numArt">Numero</label>
                     <input id="numArt" name="numArt" class="form-control" style="display: none" type="text" value="<?php echo($numArt); ?>" readonly="readonly" />
